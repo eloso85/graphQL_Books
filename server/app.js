abf0a2ea62ecd87cad5c3b414d72ff1ed6407d63
Alejandro <eloso85@gmail.com>
2020-10-21 23:25:32 -0500

    added schema

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql');
-
+const schema = require('./schema/schema');
 const app = express();
 
 // this is saying when you chose route /graphql let graphql handle request
 
 app.use('/graphql', graphqlHTTP({ 
-
+    schema
 }))
 
 app.listen(4000,()=>{
