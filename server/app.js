bb54485482adc60e187a250bd2b074548a52e38d
Alejandro <eloso85@gmail.com>
2020-10-21 23:01:50 -0500

    made schema

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const {graphqlHTTP} = require('express-graphql');
 
 const app = express();
 
+// this is saying when you chose route /graphql let graphql handle request
+
+app.use('/graphql', graphqlHTTP({ 
+
+}))
+
 app.listen(4000,()=>{
-    console.log
+    console.log('now listening today')
 })
